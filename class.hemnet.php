0465c581f977a397821019de23e75e5dff283a08
Simon Sawert <simon@sawert.se>
2016-12-08 22:44:32 +0100

    Hemnet - More effective way to render settings for the plugin

--- class.hemnet.php
+++ class.hemnet.php
@@ -4,7 +4,7 @@ Plugin Name:    Hemnet
 Plugin URI:     https://github.com/bombsimon/hemnet-plugin
 Description:    Scrape information from Hemnet
 Author:         Simon Sawert
-Version:        0.1.0
+Version:        0.2.0
 Author URI:     http://sawert.se
 License:        GPL3
 License URI:    https://www.gnu.org/licenses/gpl-3.0.html
@@ -14,7 +14,6 @@ Text Domain:    hemnet
 
 include_once '_inc/simple_html_dom.php';
 
-// Block direct requests
 if ( ! defined( 'ABSPATH' ) )
     die( '-1' );
 
@@ -105,73 +104,44 @@ class Hemnet extends WP_Widget {
     }
 
     public function form ( $instance ) {
-        $current_values = [];
+        foreach ( $this->settings() as $setting => $data ) {
+            $setting_value = $this->defined_or_fallback( $instance[$setting], $data['default-value'] );
 
-        foreach ( $this->available_settings() as $setting => $default_value ) {
-            $current_values[$setting] = $this->defined_or_fallback( $instance[$setting], $default_value );
+            if ( $data['header'] ) {
+                printf( '<p><strong>%s</strong></p>', $data['header'] );
+            }
+
+            echo '<p>';
+            if ( $data['type'] == 'text' ) {
+                printf( '<label for="%s">%s</label>', $this->get_field_id( $setting ), $data['title'] );
+                printf( '<input class="widefat" id="%s" name="%s" type="text" value="%s">', $this->get_field_id( $setting ), $this->get_field_name( $setting ), esc_attr( $setting_value ) );
+            } else if ( $data['type'] == 'select' ) {
+                printf( '<label for="%s">%s</label>', $this->get_field_id( $setting ), $data['title'] );
+                printf( '<select class="widefat" id="%s" name="%s">', $this->get_field_id( $setting ), $this->get_field_name( $setting ) );
+
+                foreach ( $data['options'] as $option => $option_value ) {
+                    $selected = $setting_value == $option ? 'selected' : '';
+                    printf( '<option value="%s" %s>%s</option>', $option, $selected, $option_value );
+                }
+                printf( '</select>' );
+
+            } else if ( $data['type'] == 'checkbox' ) {
+                $is_checked = $setting_value ? 'checked' : '';
+                printf( '<input id="%s" name="%s" type="checkbox" value="1" %s>', $this->get_field_id( $setting ), $this->get_field_name( $setting ), $is_checked );
+                printf( '<label for="%s">%s</label>', $this->get_field_id( $setting ), $data['title'] );
+            }
+
+            if ( $data['description'] ) {
+                printf( '<small>%s</small>', $data['description'] );
+            }
+            echo '</p>';
         }
-?>
-
-        <p>
-            <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'hemnet' ); ?></label>
-            <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $current_values['title'] ); ?>">
-        </p>
-
-        <p>
-            <label for="<?php echo $this->get_field_id( 'type' ); ?>"><?php _e( 'Type:', 'hemnet' ); ?></label>
-            <select class="widefat" id="<?php echo $this->get_field_id( 'type' ); ?>" name="<?php echo $this->get_field_name( 'type' ); ?>">
-                <option value="for-sale" <?php echo $current_values['type'] == 'for-sale' ? 'selected' : '' ?>><?php _e( 'For sale', 'hemnet' ) ?></option>
-                <option value="sold" <?php echo $current_values['type'] == 'sold' ? 'selected' : '' ?>><?php _ex( 'Sold', 'In settings dropdown', 'hemnet' ) ?></option>
-            </select>
-        </p>
-
-        <p>
-            <label for="<?php echo $this->get_field_id( 'location_ids' ); ?>"><?php _e( 'Location ID\'s:', 'hemnet' ); ?></label>
-            <input class="widefat" id="<?php echo $this->get_field_id( 'location_ids' ); ?>" name="<?php echo $this->get_field_name( 'location_ids' ); ?>" type="text" value="<?php echo esc_attr( $current_values['location_ids'] ); ?>">
-            <small><?php _e( 'Comma separated list of "location_ids". Search your desired location and copy the last number from the URL from Hemnet.', 'hemnet' ) ?></small>
-        </p>
-
-        <p>
-            <label for="<?php echo $this->get_field_id( 'exact_numbers' ); ?>"><?php _e( 'Exact numbers:', 'hemnet' ); ?></label>
-            <input class="widefat" id="<?php echo $this->get_field_id( 'exact_numbers' ); ?>" name="<?php echo $this->get_field_name( 'exact_numbers' ); ?>" type="text" value="<?php echo esc_attr( $current_values['exact_numbers'] ); ?>">
-            <small><?php _e( 'Comma separated list of specific numbers for a given address. Only use this with ONE location ID.', 'hemnet' ) ?></small>
-        </p>
-
-        <p>
-            <label for="<?php echo $this->get_field_id( 'max_results' ); ?>"><?php _e( 'Max results:', 'hemnet' ); ?></label>
-            <input class="widefat" id="<?php echo $this->get_field_id( 'max_results' ); ?>" name="<?php echo $this->get_field_name( 'max_results' ); ?>" type="text" value="<?php echo esc_attr( $current_values['max_results'] ); ?>">
-        </p>
-
-        <p>
-            <strong><?php _e( 'Formatting', 'hemnet' ) ?></strong>
-        </p>
-
-        <p>
-            <?php $increase_checked = $current_values['show_increase'] ? 'checked' : ''; ?>
-            <input id="<?php echo $this->get_field_id( 'show_increase'); ?>" name="<?php echo $this->get_field_name( 'show_increase' ); ?>" type="checkbox" value="1" <?php echo $increase_checked ?>>
-            <label for="<?php echo $this->get_field_id( 'show_increase' ); ?>"><?php _e( 'Display price change (only for sold)', 'hemnet' ) ?></label>
-        </p>
-
-        <p>
-            <?php $date_after_checked = $current_values['date_after_address'] ? 'checked' : ''; ?>
-            <input id="<?php echo $this->get_field_id( 'date_after_address'); ?>" name="<?php echo $this->get_field_name( 'date_after_address' ); ?>" type="checkbox" value="1" <?php echo $date_after_checked ?>>
-            <label for="<?php echo $this->get_field_id( 'date_after_address' ); ?>"><?php _e( 'Display date after address (only for sold)', 'hemnet' ) ?></label>
-        </p>
-
-        <p>
-            <?php $ppm2_checked = $current_values['show_ppm2'] ? 'checked' : ''; ?>
-            <input id="<?php echo $this->get_field_id( 'show_ppm2'); ?>" name="<?php echo $this->get_field_name( 'show_ppm2' ); ?>" type="checkbox" value="1" <?php echo $ppm2_checked ?>>
-            <label for="<?php echo $this->get_field_id( 'show_ppm2' ); ?>"><?php _e( 'Display price per m2', 'hemnet' ) ?></label>
-        </p>
-
-<?php
-
     }
 
     public function update( $new_instance, $old_instance ) {
         $instance = [];
 
-        foreach ( $this->available_settings() as $setting => $default_value ) {
+        foreach ( $this->settings() as $setting => $data ) {
             $instance[$setting] = ( ! empty( $new_instance[$setting] ) ) ? strip_tags( $new_instance[$setting] ) : '';
         }
 
@@ -182,50 +152,73 @@ class Hemnet extends WP_Widget {
         return isset( $defined ) ? $defined : $fallback;
     }
 
-    private function available_settings () {
+    private function settings () {
         $settings = [
-            'title'              => 'Hemnet',
-            'type'               => 'for-sale',
-            'location_ids'       => '882639',
-            'exact_numbers'      => '',
-            'max_results'        => '10',
-            'show_increase'      => '1',
-            'date_after_address' => '',
-            'show_ppm2'          => '1',
+            'title' => [
+                'title'         => __( 'Title:', 'hemnet' ),
+                'default-value' => 'Hemnet',
+                'type'          => 'text',
+            ],
+            'type' => [
+                'title'         => __( 'Type:', 'hemnet' ),
+                'default-value' => 'for-sale',
+                'type'          => 'select',
+                'options' => [
+                    'for-sale'  => __( 'For sale', 'hemnet' ),
+                    'sold'      => _x( 'Sold', 'In settings dropdown', 'hemnet' ),
+                ]
+            ],
+            'location_ids' => [
+                'title'         => __( 'Location ID\'s:', 'hemnet' ),
+                'default-value' => '882639',
+                'type'          => 'text',
+                'description'   => __( 'Comma separated list of "location_ids". Search your desired location and copy the last number from the URL from Hemnet.', 'hemnet' ),
+            ],
+            'exact_numbers' => [
+                'title'         => __( 'Exact numbers:', 'hemnet' ),
+                'type'          => 'text',
+                'description'   => __( 'Comma separated list of specific numbers for a given address. Only use this with ONE location ID.', 'hemnet' ),
+            ],
+            'max_results' => [
+                'title'         => __( 'Max results:', 'hemnet' ),
+                'default-value' => '10',
+                'type'          => 'text',
+            ],
+            'show_increase' => [
+                'title'         => __( 'Display price change (only for sold)', 'hemnet' ),
+                'default-value' => '1',
+                'type'          => 'checkbox',
+                'header'        => __( 'Formatting', 'hemnet' ),
+            ],
+            'date_after_address' => [
+                'title'         => __( 'Display date after address (only for sold)', 'hemnet' ),
+                'type'          => 'checkbox',
+            ],
+            'show_ppm2' => [
+                'title'         => __( 'Display price per m2', 'hemnet' ),
+                'default-value' => '1',
+                'type'          => 'checkbox',
+            ],
         ];
 
         return $settings;
     }
 
     private function scrape_hemnet ( $args ) {
-        // Location IDs @ hemnet.se - Search for your location and copy the last number/ID in the URL
-        // Rembemer to be as specific as possible, this will only show the first 50 results (first result page on hemnet.se)
-        $location_ids = split( ',', $args['location_ids'] );
-
-        // Exact match, only match these numbers from the results (hemnet.se does not support specific numbers so we just filter everything else out)
-        // This should not be used when using multiple location IDs since we don't know which of the addresses the number should be fixed for
+        $location_ids  = split( ',', $args['location_ids'] );
         $exact_numbers = $args['exact_numbers'] ? split( ',', $args['exact_numbers'] ) : [];
 
-        // Object result placeholder
-        $objects = [];
-
-        // Attributes to fetch from each result object
+        $objects    = [];
         $attributes = $this->get_attributes();
 
-        // Fallback for errors
         if ( ! $attributes )
             return $objects;
 
-        // Set scrape type
         $type = $args['type'];
 
-        // The Hemnet address
         $hemnet_address = sprintf( 'http://www.hemnet.se/%sbostader?%s', $attributes['address-extra'][$type], join( '&', array_map( function( $id ) { return sprintf( 'location_ids[]=%d', $id ); }, $location_ids ) ) );
+        $dom            = @file_get_html( $hemnet_address );
 
-        // Get DOM from Hemnet - supress warnings because reasons...
-        $dom = @file_get_html( $hemnet_address );
-
-        // Return empty object list if request fails
         if ( ! $dom )
             return $objects;
 
@@ -233,24 +226,17 @@ class Hemnet extends WP_Widget {
             foreach ( $attributes['data-classes'] as $key => $value ) {
                 $data = $item->find( $value[$type], 0 );
 
-                // Get plaintext except for URLs
                 $value = $data->plaintext;
 
-                // Get href if we're looking for URL
-                // The reuslt list for sold items contain full link, the list for items for sale does not...
                 if ( $key == 'url' )
                     $value = sprintf( '%s%s', $args['type'] == 'for-sale' ? 'http://www.hemnet.se' : '', $data->href );
 
-                // Get data-src if we're looking for image
                 if ( $key == 'image' )
                     $value = $data->{'data-src'};
 
-                // Some text cleanup...
                 $value = preg_replace( '/&nbsp;/', ' ', $value );
                 $value = preg_replace( '/^\s+|\s+$/', '', $value );
                 $value = preg_replace( '/\s{2,}/', ' ', $value );
-
-                // And remove hard coded pre- and postfixes
                 $value = preg_replace( '/Begärt pris: /', '', $value );
                 $value = preg_replace( '/Såld /', '', $value );
                 $value = preg_replace( '/Slutpris /', '', $value );
@@ -264,7 +250,7 @@ class Hemnet extends WP_Widget {
                     preg_match( '/^([\d,]+) \D+ ([\d,]+) rum/', $value, $size_info );
 
                     $objects[$i]['living-area'] = $size_info[1];
-                    $objects[$i]['rooms'] = $size_info[2];
+                    $objects[$i]['rooms']       = $size_info[2];
 
                     continue;
                 }
@@ -275,27 +261,24 @@ class Hemnet extends WP_Widget {
             $i++;
         }
 
-        // Return all (max 50) matches if no number filter is set
         if ( ! count( $exact_numbers ) )
             return $objects;
 
-        // Fiter exact matches for specific numbers, still from the max 50 results
-        $final = [];
+        $exact_matches = [];
         foreach ( $objects as $obj ) {
             foreach ( $exact_numbers as $exact ) {
-                // Sadly there are no standard for addresses so this should match what's the street number in the string
                 preg_match( '/^(\D+) (\d+)\w?(,|$)/', $obj['address'], $address );
 
                 if ( $address[2] == $exact ) {
-                    $final[] = $obj;
+                    $exact_matches[] = $obj;
                 }
             }
         }
 
-        return $final;
+        return $exact_matches;
     }
 
-    private function format_date( $date = '00 januari 1990' ) {
+    private function format_date( $date = '1 januari 1990' ) {
         $m = [
             'januari'   => 1,
             'februari'  => 2,
